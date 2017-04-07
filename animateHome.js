5bdc0722a9b85e4d65d5592e5752bd840ddbe99c
Robert Barker <RobertBarker@Roberts-MacBook-Pro.local>
2017-04-07 15:19:32 +0300

    First commit

---
+++ animateHome.js
@@ -0,0 +1,9 @@
+/**
+ * Created by RobertBarker on 3/30/17.
+ */
+$(document).ready(function() {
+    $('select').material_select();
+    $('.button-collapse').sideNav();
+    $('.scrollspy').scrollSpy();
+    $('.modal').modal();
+});
---
+++ animateResults.js
@@ -0,0 +1,6 @@
+/**
+ * Created by RobertBarker on 4/5/17.
+ */
+$(document).ready(function() {
+    $(".button-collapse").sideNav();
+});
